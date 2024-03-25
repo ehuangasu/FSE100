0223042f441e45398976906a84fd196afed624d6
ehuangasu <163350480+ehuangasu@users.noreply.github.com>
2024-03-25 11:19:47 -0900

    Update PIR_buzzer.py

--- PIR_buzzer.py
+++ PIR_buzzer.py
@@ -2,25 +2,22 @@
 import RPi.GPIO as GPIO
 import time
 
-PirPin = 11
-Buzzpin = 12
-test = 1
+PIR = 11
+BUZZ = 12
 
+# Setup board
 def setup():
 	GPIO.setmode(GPIO.BOARD)          # Numbers GPIOs by physical location
-	GPIO.setup(Buzzpin, GPIO.OUT)     # Set Buzzer Led Pin mode to output
-	GPIO.setup(PirPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
-	GPIO.add_event_detect(PirPin, GPIO.BOTH, callback=detect)
-	off();
-	beep(0.5);
+	GPIO.setup(BUZZ, GPIO.OUT)     # Set Buzzer Led Pin mode to output
+	GPIO.setup(PIR, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
+	GPIO.add_event_detect(PIR, GPIO.BOTH, callback=detect, bouncetime=200)
+	off(); # Ensure buzzer is off
 	
 def on():
-	print('on')
-	GPIO.output(Buzzpin, GPIO.LOW)
+	GPIO.output(BUZZ, GPIO.LOW)
 
 def off():
-	print('off')
-	GPIO.output(Buzzpin, GPIO.HIGH)
+	GPIO.output(BUZZ, GPIO.HIGH)
 
 def beep(x):
 	on()
@@ -29,12 +26,9 @@ def beep(x):
 	time.sleep(x)
 
 def detect(chn):
-	global test
-	print('detect' + str(GPIO.input(PirPin)))
-	test = GPIO.input(PirPin);
-	if GPIO.input(PirPin) == 0:
+	if GPIO.input(PIR) == 0:
 		on()
-	if GPIO.input(PirPin) == 1:
+	if GPIO.input(PIR) == 1:
 		off()
 
 def loop():
@@ -42,8 +36,8 @@ def loop():
 		pass
 
 def destroy():
-	GPIO.output(Buzzpin, GPIO.HIGH)       # Buzzer off
-	GPIO.cleanup()                        # Release resource
+	GPIO.output(BUZZ, GPIO.HIGH)       # Buzzer off
+	GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
 	setup()
